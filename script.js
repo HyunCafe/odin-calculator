44f7373ee7a9a938b5380d98fb75c2f1d83e4acd
HyunCafe <eparksuncafe@gmail.com>
2023-02-02 23:21:13 -0600

    Feat: Added button inputs show to display n scientific notation

--- script.js
+++ script.js
@@ -20,5 +20,26 @@
 // OR have a little history button that pops up a short display of history calculations
 
 // Have all inputs visually display on the display class
+const buttons = document.querySelectorAll(".calc-cell");
+const display = document.querySelector(".display");
+
+function displayResult(result) {
+  display.textContent = result;
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let text = button.textContent;
+    if (display.textContent.length >= 18) {
+      let result = parseFloat(display.textContent + text).toExponential();
+      if (result.length > 20) {
+        result = "..." + result.substring(result.length - 15, result.length);
+      }
+      displayResult(result);
+    } else {
+      display.textContent += text;
+    }
+  });
+});
 
 // Maybe have a cute gif/animation play around the top borders of the calculator
