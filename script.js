55cd6cafb90e5a6b31bcc41433679cbfae4f8dea
HyunCafe <eparksuncafe@gmail.com>
2023-02-02 18:29:06 -0600

    Add: Added pseudo code and ideas to implement

---
+++ script.js
@@ -0,0 +1,24 @@
+// Clear Display Function
+
+// Delete last input function
+
+// percentage function
+
+// division function
+
+// multiply function
+
+// subtraction function
+
+// addition function
+
+// equal calculate
+
+// create maximum input value for display
+
+// have a short history of passed calculations show up toward the top and disappear as more inputs come in a scrolling like effect
+// OR have a little history button that pops up a short display of history calculations
+
+// Have all inputs visually display on the display class
+
+// Maybe have a cute gif/animation play around the top borders of the calculator
