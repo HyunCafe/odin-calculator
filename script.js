5a0b817e44305df8d678414573dcdf9f8a5350ef
HyunCafe <eparksuncafe@gmail.com>
2023-02-07 18:49:52 -0600

    Fix: Fix bug where multiple calculations does not update new value with new calculation

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const buttons = document.querySelectorAll(".calc-cell");
 const display = document.querySelector(".display");
 const smallerDisplay = document.querySelector(".small-display");
 
-let num1 = '';
-let num2 = '';
-let operator = '';
-let result = '';
+let num1 = "";
+let num2 = "";
+let operator = "";
+let result = "";
 
 // Main Display Function
 const displayResult = (value) => {
@@ -14,24 +14,24 @@ const displayResult = (value) => {
 };
 
 // Small Display Function
-let smallDisplayText = '';
+let smallDisplayText = "";
 const smallDisplay = (text) => {
   smallDisplayText += text;
   smallerDisplay.textContent = smallDisplayText;
-}
+};
 
 // Clear the main display
 const clearDisplay = () => {
-  num1 = '';
-  num2 = '';
-  operator = '';
-  result = '';
-  displayResult('');
-  smallDisplayText = '';
-  smallerDisplay.textContent = '';
+  num1 = "";
+  num2 = "";
+  operator = "";
+  result = "";
+  displayResult("");
+  smallDisplayText = "";
+  smallerDisplay.textContent = "";
 };
 
-// Delete last input function 
+// Delete last input function
 function deleteButton() {
   display.textContent = display.textContent.slice(
     0,
@@ -46,6 +46,7 @@ const operands = (e) => {
     num1 += value;
     displayResult(num1);
   } else {
+    num2 = "";
     num2 += value;
     displayResult(`${num1} ${operator} ${num2}`);
   }
@@ -54,35 +55,47 @@ const operands = (e) => {
 // Operator Values
 const operators = (e) => {
   operator = e.target.textContent;
+  currentValue = null;
   displayResult(`${num1} ${operator}`);
   smallDisplay(` ${operator} `);
 };
 
 const calculate = () => {
-  const n1 = +num1;
-  const n2 = +num2;
+  let n1 = +num1;
+  let n2 = +num2;
+  if (!n2) {
+    n2 = +display.textContent;
+  }
   switch (operator) {
     case "+":
-      result = n1 + n2;
+      finalValue = n1 + n2;
       break;
     case "-":
-      result = n1 - n2;
+      finalValue = n1 - n2;
       break;
     case "*":
-      result = n1 * n2;
+      finalValue = n1 * n2;
       break;
     case "/":
-      result = n1 / n2;
-      case "%":
-      result = n1 / 100 * n2;
+      finalValue = n1 / n2;
+      break;
+    case "%":
+      finalValue = (n1 / 100) * n2;
       break;
     default:
-      result = '';
+      finalValue = "";
   }
-  displayResult(result);
-  smallDisplayText = `${n1} ${operator} ${n2} = `;
-  smallDisplay('');
+  displayResult(finalValue);
+  smallDisplayText = `${n1} ${operator} ${n2} =`;
+  smallDisplay(smallDisplayText);
+  num1 = finalValue;
+  num2 = null;
+  operator = null;
 };
+
+let previousValue = null;
+let currentValue = null;
+let finalValue = null;
 
 buttons.forEach((button) => {
   if (button.textContent === "AC") {
@@ -92,18 +105,26 @@ buttons.forEach((button) => {
   } else if (button.textContent.match(/\d/)) {
     button.addEventListener("click", operands);
   } else if (button.textContent.match(/[\+\-\*\%\/]/)) {
-    button.addEventListener("click", (e) => operators(e, smallDisplay(display.textContent + ` ${operator} ` + num2)));
-  } else if (button.textContent === "=") {
-    button.addEventListener("click", () => {
-      calculate();
-      num1 = result;
-      operator = null;
-      num2 = '';
+    button.addEventListener("click", (e) => {
+      if (previousValue && operator) {
+        // Store the new values and disregard the old values
+        previousValue = finalValue;
+        currentValue = null;
+        operator = e.target.innerText;
+      } else {
+        // Store the first values
+        previousValue = +display.textContent;
+        currentValue = null;
+        operator = e.target.innerText;
+      }
+      smallDisplay(`${previousValue} ${operator}`);
     });
+  } else if (button.textContent === "=") {
+    button.addEventListener("click", calculate);
   }
 });
 
-// Bugs 
+// Bugs
 //Bug 1: History Button disappears on AC clear because its connect to small display
 //Bug 2: Repeated press of operators display on small window, want it only once
 //Bug 3: set toFixed(4) decimal places for results, currently too many decimals
@@ -113,14 +134,12 @@ buttons.forEach((button) => {
 // Add Features
 //Feat 1: Add history button with working history on new window --
 // to fix this issue I think i will just add the history button outside of the display
-// but for aesthetics I need to create a top bar, I will copy the windows default 
+// but for aesthetics I need to create a top bar, I will copy the windows default
 // Calculator layout for this
-
-
-//Feat 2: cute gif/animation play around the top borders of the calculator 
+//Feat 2: cute gif/animation play around the top borders of the calculator
 //Feat 3: Re-add feature of:
-    // Check if the display text is equal to infinity
-        // if num > 20 Convert the result to exponential notation
-            // If the length of the result is less than or equal to 20
-                // If length >20 Display last 15 chars of the result preceded by "..."
+// Check if the display text is equal to infinity
+// if num > 20 Convert the result to exponential notation
+// If the length of the result is less than or equal to 20
+// If length >20 Display last 15 chars of the result preceded by "..."
 //Feat 4: repeatedly pressing equal button keeps doing the last operation
